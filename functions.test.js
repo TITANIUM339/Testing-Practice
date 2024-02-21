f1169556168c3d0e5d62eb637a4a2662e4d64778
TITANIUM <bobofofo576@gmail.com>
2024-02-21 15:32:18 +0300

    Add test for reverseString function

--- functions.test.js
+++ functions.test.js
@@ -1,7 +1,13 @@
-const { capitalize } = require("./functions.js");
+const { capitalize, reverseString } = require("./functions.js");
 
 test("capitalize", () => {
     expect(capitalize("hello, world!")).toBe("Hello, world!");
     expect(capitalize("anIMaL")).toBe("AnIMaL");
     expect(capitalize("Chocolate")).toBe("Chocolate");
+});
+
+test("reverseString", () => {
+    expect(reverseString("hello, world!")).toBe("!dlrow ,olleh");
+    expect(reverseString("anIMaL")).toBe("LaMIna");
+    expect(reverseString("Chocolate")).toBe("etalocohC");
 });
