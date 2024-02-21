93f7c4145b168bec2e16a62219c0c6459bb63620
TITANIUM <bobofofo576@gmail.com>
2024-02-21 17:23:54 +0300

    Add test for calculator methods

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString } = require("./functions.js");
+const { capitalize, reverseString, calculator } = require("./functions.js");
 
 test("capitalize", () => {
     expect(capitalize("hello, world!")).toBe("Hello, world!");
@@ -10,4 +10,11 @@ test("reverseString", () => {
     expect(reverseString("hello, world!")).toBe("!dlrow ,olleh");
     expect(reverseString("anIMaL")).toBe("LaMIna");
     expect(reverseString("Chocolate")).toBe("etalocohC");
+});
+
+test("calculator", () => {
+    expect(calculator.add(1, 2)).toBe(3);
+    expect(calculator.subtract(0, 1)).toBe(-1);
+    expect(calculator.divide(10, 2)).toBe(5);
+    expect(calculator.multiply(10, 10)).toBe(100);
 });
